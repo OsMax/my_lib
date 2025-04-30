88437adf4d480ebd69fdf88c6a4225de3e9fb0c3
OsMax <www.osmax@gmail.com>
2025-04-30 13:03:22 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,7 @@
-import { NavLink } from "react-router-dom";
-import { useState } from "react";
-import css from "./Header.module.css";
-
-const setAtive = ({ isActive }) => (isActive ? css.active : css.navLink);
 const Header = () => {
-  const [showMenu, setShowMenu] = useState(false);
-
   return (
-    <header className={css.header}>
-      <div className={css.headerContainer}></div>
+    <header>
+      <div></div>
     </header>
   );
 };
