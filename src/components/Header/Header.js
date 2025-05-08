fe3337df06732f4ae7bc37adc2a6ad2268466c5d
OsMax <www.osmax@gmail.com>
2025-05-08 08:45:28 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const setAtive = ({ isActive }) => (isActive ? css.active : css.navLink);
 const Header = () => {
   return (
     <header>
-      <div>Header test</div>
+      <div>Header test 2</div>
     </header>
   );
 };
