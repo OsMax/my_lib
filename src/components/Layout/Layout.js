d96f09b1b9b3ebbf8f2d8a417e2d6366aa81eb8c
OsMax <www.osmax@gmail.com>
2025-05-08 09:49:03 +0300

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,13 +2,15 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Main from "../Main/Main";
 
+import css from "./Layout.module.css";
+
 const Layout = () => {
   return (
-    <>
+    <div className={css.mainContainer}>
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 };
 
