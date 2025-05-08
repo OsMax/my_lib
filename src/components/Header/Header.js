39811a67a06b0adf6c75892336871c8a0f6e7182
OsMax <www.osmax@gmail.com>
2025-05-08 09:01:10 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
-import { NavLink } from "react-router-dom";
-import { useState } from "react";
-import css from "./Header.module.css";
-import { ReactComponent as Burger } from "../../assets/svgImg/burger.svg";
-import { ReactComponent as Close } from "../../assets/svgImg/menu_close.svg";
-import { ReactComponent as Logo } from "../../assets/svgImg/logo.svg";
+// import { NavLink } from "react-router-dom";
+// import { useState } from "react";
+// import css from "./Header.module.css";
+// import { ReactComponent as Burger } from "../../assets/svgImg/burger.svg";
+// import { ReactComponent as Close } from "../../assets/svgImg/menu_close.svg";
+// import { ReactComponent as Logo } from "../../assets/svgImg/logo.svg";
 
-const setAtive = ({ isActive }) => (isActive ? css.active : css.navLink);
+// const setAtive = ({ isActive }) => (isActive ? css.active : css.navLink);
 
 const Header = () => {
   return (
